bd83a8fa64c77fee95329f1d3ceadbc00b5d4f3d
Riccardo Vannetti <riccardo.vannetti@comune.fi.it>
2020-08-12 11:36:12 +0200

    version 1.3.5 , update divo-tag.sh

--- divo-tag.sh
+++ divo-tag.sh
@@ -2,9 +2,6 @@ echo "**Divo-Tag**"
 VERSION=$1
 echo '[DT] going to set version chip to... ' $VERSION
 sed -i 's/        divo_version:.*/        divo_version: "'$VERSION'"/' config/packages/twig.yaml
-echo '[DT] going to composer update.. '
-composer update
-echo '[DT] going to yarn encore prod... '
-yarn encore prod
+sed -i 's/softwareVersion: .*/softwareVersion: '$VERSION'/' publiccode.yml
 echo '[DT] Completed.'
 echo '[DT] You can proceed to commit, tag and push your new version '$VERSION' .'
